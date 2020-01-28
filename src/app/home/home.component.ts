c841db447019cd3b6b8d7fc2f0e0bfd3ae63d5dd
idawud <ismaildawud96@gmail.com>
2020-01-28 09:55:41 +0000

    fix for deploy 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,7 +33,7 @@ export class HomeComponent implements OnInit{
 
   ngOnInit() {
     this.userEmail = this.cookieService.get('ttpEmail');
-    this.userName  = this.cookieService.get
+    // this.userName  = this.cookieService.get
 
     this.service.getRolesListing().subscribe(
       result => {
