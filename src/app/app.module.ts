134fcf9c49f2b8748d96bf4abe5691b604e7feca
patriciakasapa <patriciakasapa07@gmail.com>
2020-02-03 16:58:58 +0000

    Review the following

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA } from '@angular/core';
+
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {MultiSelectModule} from 'primeng/multiselect';
+import {ButtonModule} from 'primeng/button';
+import { HttpClientModule } from '@angular/common/http';
+import { AppserviceService } from './appservice.service';
+
+// Cookie stuff
+import {CookieService} from 'ngx-cookie-service';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
+ 
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule, MultiSelectModule,
+    ButtonModule, BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule, 
+    BrowserAnimationsModule, 
+    MatCheckboxModule,
+    NgMultiSelectDropDownModule.forRoot()
+  ],
+  providers: [
+  CookieService,
+  AppserviceService
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
+  bootstrap: [AppComponent, 
+  ]
+})
+export class AppModule { }
