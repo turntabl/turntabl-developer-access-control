af3b854ee7f8909dcf046e718d06404f767a494f
patriciakasapa <patriciakasapa07@gmail.com>
2020-02-03 16:51:56 +0000

    Review the following

--- src/app/appservice.service.ts
+++ src/app/appservice.service.ts
@@ -1,11 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { HomeComponent } from './home/home.component';
 import { RolesList } from './roles-list';
 import { RoleRequest } from './role-request';
-import { PendingRequests } from './pending-requests';
-import { BehaviorSubject } from 'rxjs';
+
 
 const httpOptions = {
   headers: new HttpHeaders({
@@ -18,22 +16,14 @@ const httpOptions = {
 })
 export class AppserviceService {
 
-  private apiURL = 'https://permission.services.turntabl.io/v1/api/aws-mgnt/';
+  private apiURL = 'process.env.PERMS';
   constructor(private httpClient: HttpClient) { }
 
   getRolesListing(): Observable<RolesList[]> {
     return this.httpClient.get<RolesList[]>(this.apiURL + 'roles');
   }
-  getPendingPermission(): Observable<PendingRequests[]> {
-    return this.httpClient.get<PendingRequests[]>(this.apiURL + 'pending');
-  }
+  
   postRequest(roleRequest: RoleRequest): Observable<RoleRequest> {
     return this.httpClient.post<RoleRequest>(this.apiURL + 'send', roleRequest, httpOptions);
   }
-
-  // private messageSource = new BehaviorSubject <string>("default message");
-  // currentMessage = this.messageSource.asObservable();
-  // changeMessage(message: string) {
-  //   this.messageSource.next(message)
-  // }
 }
